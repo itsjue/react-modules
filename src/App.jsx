c6ca7980213cbab4529d6f861671e88ee9080056
PHET45 <phnjk2000@gmail.com>
2025-08-04 14:40:06 +0700

    refactor: update import statements in App component to use named imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import ProductSection from "./components/ProductSection";
-import Footer from "./components/Footer";
-import FAQ from "./components/FAQ";
+import {Header} from './components/Header';
+import {MainContent} from './components/MainContent';
+import {ProductSection} from "./components/ProductSection";
+import {Footer} from "./components/Footer";
+import {FAQ} from "./components/FAQ";
 
 
 
